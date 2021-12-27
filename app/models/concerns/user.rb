2fe4a82146ed9a347a52df15f670b52b2b55b329
Byron Hassim <byronhassim@gmail.com>
2021-12-27 08:14:49 +0200

    Add user data to articles listing page

--- app/models/concerns/user.rb
+++ app/models/concerns/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
              length: { minimum: 3, maximum: 25 }
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :email, presence: true, length: { maximum: 105 }
-          uniqueness: { case_sensitive: false },
-          format: { with: VALID_EMAIL_REGEX }
+          #uniqueness: { case_sensitive: false },
+          #format: { with: VALID_EMAIL_REGEX }
 end
